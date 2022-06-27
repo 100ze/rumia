24098743249d4c065f2ec30b510a72e15a24c97f
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2022-06-27 12:23:14 -0300

    navegador: botão enviar

--- public/js/navegador.js
+++ public/js/navegador.js
@@ -12,15 +12,10 @@ class Navegador {
   static _criar_guia_padrao(id) {
     const guia_wrapper = document.createElement("div")
     const guia = ` 
-      <section class="r_pesquisa row justify-content-center">
+      <section class="r_enviar js_guia_padrao row justify-content-center">
         <div class="col-11 row">
-          <input class="r_campo_de_pesquisa js_campo_de_pesquisa col border-0 fs-2" placeholder="http://">
-          <button class="r_botao_pesquisar js_botao_pesquisar col-4 btn border-0 rounded-0 fs-1">Go!</button>
-        </div>
-      </section>
-      <section class="row mt-2 justify-content-center">
-        <div class="col-11 row">
-          <button class="col r_botao_incorporar js_botao_incorporar btn rounded-0 fs-1">Incorporar</button>
+          <input class="r_campo js_campo col border-0 fs-2" placeholder="http://">
+          <button class="r_botao_enviar js_botao_enviar col-4 btn border-0 rounded-0 fs-1">Go!</button>
         </div>
       </section>
     `
@@ -30,16 +25,43 @@ class Navegador {
     return guia_wrapper
   }
 
-  static _oi() {
-    window.console.log("oi")
+  static _criar_iframe(endereco) {
+    const iframe = document.createElement("iframe")
+
+    iframe.classList.add("r_iframe")
+
+    iframe.src = endereco
+
+    return iframe
+  }
+
+  static _corrigir_endereco(endereco) {
+    const https = endereco.includes("https://")
+    const http = endereco.includes("http://")
+
+    if (http || https) {
+      return endereco
+    } else {
+      return "https://" + endereco
+    }
+  }
+
+  static _botao_enviar() {
+    const guia = this.parentNode.parentNode.parentNode
+    const campo = guia.querySelector(".js_campo")
+    const endereco = Navegador._corrigir_endereco(campo.value)
+    const iframe = Navegador._criar_iframe(endereco)
+
+    guia.innerHTML = ""
+
+    guia.appendChild(iframe)
   }
 
   static _adicionar_funcionalidade(guia) {
-    const botao_pesquisar = guia.querySelector(".js_botao_pesquisar")
-    const campo_de_pesquisa = guia.querySelector(".js_campo_de_pesquisa")
-    const botao_incorporar = guia.querySelector(".js_botao_incorporar")
+    const botao_enviar = guia.querySelector(".js_botao_enviar")
+    const campo = guia.querySelector(".js_campo")
 
-    botao_pesquisar.addEventListener("click", this._oi)
+    botao_enviar.addEventListener("click", this._botao_enviar)
   }
 
   static nova_guia() {
@@ -47,6 +69,8 @@ class Navegador {
     const guia = this._criar_guia_padrao(numero_da_guia)
 
     this._adicionar_funcionalidade(guia)
+
+    this._lista_de_guias.push(guia)
 
     this._lista_de_iframes.appendChild(guia)
 
