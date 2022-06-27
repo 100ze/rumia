45df2d6bd3c1c72d48e065b315d3643102f07f11
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2022-06-27 02:53:49 -0300

    wrapper: função adicionar shaddow dom

--- public/js/rumia.js
+++ public/js/rumia.js
@@ -6,14 +6,20 @@ class Wrapper {
     this.body = document.querySelector("body")
   }
 
-  static criar_wrapper() {
-    const wrapper = document.createElement("div")
-    wrapper.classList.add("rumia_wrapper")
-    this.body.appendChild(wrapper)
+  static _adicionar_ao_shadow_dom(elemento, conteudo) {
+    elemento.attachShadow({ mode: "open" }).innerHTML = conteudo
+  }
 
-    return wrapper
+  static criar_wrapper(conteudo) {
+    const wrapper = document.createElement("div")
+
+    wrapper.classList.add("rumia_wrapper")
+
+    this._adicionar_ao_shadow_dom(wrapper, conteudo)
+
+    this.body.appendChild(wrapper)
   }
 }
 
 // testes manuais
-Wrapper.criar_wrapper()
+Wrapper.criar_wrapper("<h1>said i love u but i lied</h1>")
