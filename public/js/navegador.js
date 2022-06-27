088405db6224724c767d205be8d987b516377741
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2022-06-27 11:13:34 -0300

    navegador: função adicionar funcionalidade

--- public/js/navegador.js
+++ public/js/navegador.js
@@ -14,13 +14,13 @@ class Navegador {
     const guia = ` 
       <section class="r_pesquisa row justify-content-center">
         <div class="col-11 row">
-          <input class="r_campo_de_pesquisa col border-0 fs-2" placeholder="http://">
-          <button class="r_botao_pesquisar col-4 btn border-0 rounded-0 fs-1">Go!</button>
+          <input class="r_campo_de_pesquisa js_campo_de_pesquisa col border-0 fs-2" placeholder="http://">
+          <button class="r_botao_pesquisar js_botao_pesquisar col-4 btn border-0 rounded-0 fs-1">Go!</button>
         </div>
       </section>
       <section class="row mt-2 justify-content-center">
         <div class="col-11 row">
-          <button class="col r_botao_incorporar btn rounded-0 fs-1">Incorporar</button>
+          <button class="col r_botao_incorporar js_botao_incorporar btn rounded-0 fs-1">Incorporar</button>
         </div>
       </section>
     `
@@ -30,10 +30,25 @@ class Navegador {
     return guia_wrapper
   }
 
+  static _oi() {
+    window.console.log("oi")
+  }
+
+  static _adicionar_funcionalidade(guia) {
+    const botao_pesquisar = guia.querySelector(".js_botao_pesquisar")
+    const campo_de_pesquisa = guia.querySelector(".js_campo_de_pesquisa")
+    const botao_incorporar = guia.querySelector(".js_botao_incorporar")
+
+    botao_pesquisar.addEventListener("click", this._oi)
+  }
+
   static nova_guia() {
     const numero_da_guia = this._lista_de_guias.length
+    const guia = this._criar_guia_padrao(numero_da_guia)
 
-    this._lista_de_iframes.appendChild(this._criar_guia_padrao(numero_da_guia))
+    this._adicionar_funcionalidade(guia)
+
+    this._lista_de_iframes.appendChild(guia)
 
     this._guia_atual = numero_da_guia
   }
